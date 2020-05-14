5ea59432382453eab07e0426f11d7b7af8c7c640
Doug Ritchie <dritchie3@sbcglobal.net>
2020-05-14 16:44:47 -0500

    add to heroku fix workout required

--- models/workout.js
+++ models/workout.js
@@ -3,54 +3,56 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema(
-    {
-        day: {
-            type: Date,
-            default: () => new Date()
-        },
-        execises: [
-            {
-                type: {
-                    type: String,
-                    trim: true,
-                    required: "Enter Exercise Type"
-                },
-                name: {
-                    type: String,
-                    trim: true,
-                    required: "Enter Exercise Name"
-                },
-                duration: {
-                    type: Number,
-                    required: "Enter Exercise Duration in Minutes"
-                },
-                weight: {
-                    type: Number
-                },
-                reps: {
-                    type: Number
-                },
-                sets: {
-                    type: Number
-                },
-                distance: {
-                    type: Number
-                }
-            }
-        ]
+  {
+    day: {
+      type: Date,
+      default: () => new Date()
     },
-    {
-        toJSON: {
-            // includes virtual properties when data is requested
-            virtuals: true
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise type"
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "Enter an exercise name"
+        },
+        duration: {
+          type: Number,
+          required: "Enter an exercise duration in minutes"
+        },
+        weight: {
+          type: Number
+        },
+        reps: {
+          type: Number
+        },
+        sets: {
+          type: Number
+        },
+        distance: {
+          type: Number
         }
+      }
+    ]
+  },
+  {
+    toJSON: {
+      // include any virtual properties when data is requested
+      virtuals: true
     }
+  }
 );
-// reduce dynamically created property of schema to the sum of thier durations
+
+// adds a dynamically-created property to schema
 workoutSchema.virtual("totalDuration").get(function () {
-    return this.exercises.reduce((total, exercise) => {
-        return total + exercise.duration;
-    }, 0);
+  // "reduce" array of exercises down to just the sum of their durations
+  return this.exercises.reduce((total, exercise) => {
+    return total + exercise.duration;
+  }, 0);
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
